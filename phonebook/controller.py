381262c511d1fc1bc64786df5f8231db5f659c2c
alekcdn <alex3l@mail.ru>
2022-10-19 17:00:08 +0300

    add HomeWork7

---
+++ phonebook/controller.py
@@ -0,0 +1,21 @@
+import function
+import view
+import UI
+
+def start():
+    button = UI.menu()
+    if button ==1:
+        view.show(function.create_data())
+        start()
+    elif button == 2:
+        function.get_data()
+    elif button == 3:
+        function.save_data()
+    elif button == 4:
+        print('До свидания!')
+    else:
+        print('Выберите цифру меню')
+        start()
+        
+        
+        
