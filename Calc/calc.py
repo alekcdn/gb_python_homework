00e757b525f8d1064c7adc05f2ec8e56e8a5a444
alekcdn <alex3l@mail.ru>
2022-11-09 18:00:39 +0300

    edit calc.py from HW9

--- Calc/calc.py
+++ Calc/calc.py
@@ -1,4 +1,3 @@
-from aiogram import Bot, Dispatcher, executor, types
 """
 This is a echo bot.
 It echoes any incoming text messages.
@@ -9,7 +8,7 @@ import csv
 
 from aiogram import Bot, Dispatcher, executor, types
 
-API_TOKEN = '5601609648:AAFPb916orpiq9AiT0hOe_wF-hsnz5fpVMo'
+API_TOKEN = ''
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
@@ -26,25 +25,10 @@ async def send_welcome(message: types.Message):
     """
     await message.reply("Hi!\nI'm EchoBot!\nPowered by aiogram.")
 
-
-# @dp.message_handler(regexp='(^cat[s]?$|puss)')
-# async def cats(message: types.Message):
-#     with open('data/cats.jpg', 'rb') as photo:
-#         '''
-#         # Old fashioned way:
-#         await bot.send_photo(
-#             message.chat.id,
-#             photo,
-#             caption='Cats are here 😺',
-#             reply_to_message_id=message.message_id,
-#         )
-#         '''
-
-#         await message.reply_photo(photo, caption='Cats are here 😺')
         
 @dp.message_handler(commands=['calc'])
 async def calc(message: types.Message):
-    # await message.reply(f'Result')
+
     data = []
     bookreader = csv.reader([message.text], delimiter=' ', quotechar='"')
 
@@ -52,26 +36,11 @@ async def calc(message: types.Message):
         data.append([row[0], row[1]])
 
     del data[0][0]
-    # eval(data[0])
     await message.reply(f'Result =  {eval(data[0][0])}')
     
-    # with open('data/cats.jpg', 'rb') as photo:
-    #     '''
-    #     # Old fashioned way:
-    #     await bot.send_photo(
-    #         message.chat.id,
-    #         photo,
-    #         caption='Cats are here 😺',
-    #         reply_to_message_id=message.message_id,
-    #     )
-    #     '''
-
-    #     await message.reply_photo(photo, caption='Cats are here 😺')
         
 @dp.message_handler()
 async def echo(message: types.Message):
-    # old style:
-    # await bot.send_message(message.chat.id, message.text)
 
     await message.answer(message.text)
 
